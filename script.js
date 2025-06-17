be3ac063b3333526663fc1b94c552fa03400807d
Rathd Pruthviraj <145155770+Rathodpruthvirajsing@users.noreply.github.com>
2025-06-17 13:37:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
 function minDate(dates) {
-  //write you code here
+  // Convert each string to a Date object
+  const dateObjects = dates.map(dateStr => new Date(dateStr.replace(/\//g, '-')));
+
+  // Find the minimum date
+  let min = dateObjects[0];
+  for (let date of dateObjects) {
+    if (date < min) {
+      min = date;
+    }
+  }
+
+  // Return as YYYY/MM/DD
+  return min.toISOString().split('T')[0].replace(/-/g, '/');
 }
 
-// Do not change the code
+function findMinDate() {
+  const input = document.getElementById('dateInput').value;
+  const dateArray = input.split(',').map(d => d.trim());
 
-var dates = [
-  "2023/03/01",
-  "2023/03/02",
-  "2023/03/03",
-  "2023/03/04",
-  "2023/03/05",
-  "2023/03/06",
-  "2023/03/07",
-  "2023/03/08",
-  "2023/03/09",
-  "2023/03/10",
-  "2023/03/11",
-  "2023/03/12",
-  "2023/03/13",
-  "2023/03/14",
-  "2023/03/15",
-  "2023/03/16",
-  "2023/03/17",
-  "2023/03/18",
-  "2023/03/19",
-  "2023/03/20",
-  "2023/03/21",
-  "2023/03/22",
-  "2023/03/23",
-  "2023/03/24",
-  "2023/03/25",
-  "2023/03/26",
-  "2023/03/27",
-  "2023/03/28",
-  "2023/03/29",
-  "2023/03/30",
-];
+  if (dateArray.length === 0 || dateArray[0] === "") {
+    document.getElementById('result').innerText = "Please enter valid dates.";
+    return;
+  }
 
-alert(minDate(dates));
+  const earliest = minDate(dateArray);
+  document.getElementById('result').innerText = "Earliest Date: " + earliest;
+}
